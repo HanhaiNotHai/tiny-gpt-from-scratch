57b129a989edebb21aa3f8813f3915a4a80e1dfe
Hanhai <1932232849@qq.com>
2026-07-16 11:27:38 +0800

    Step 36: pick_split_point

--- model.py
+++ model.py
@@ -234,8 +234,11 @@ def encode_corpus_to_int_array(text: str, stoi: dict[str, int]):
 
     return np.array(encode_string(text, stoi), dtype=np.int64)
 
-# Step 36 - pick_split_point (not yet solved)
-# TODO: implement
+# Step 36 - pick_split_point
+def pick_split_point(n: int, train_frac: float):
+    """Return integer split index so data[:idx] is train and data[idx:] is val."""
+
+    return int(n * train_frac)
 
 # Step 37 - slice_train_and_val (not yet solved)
 # TODO: implement
